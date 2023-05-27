9028dfba0f59be659a75736e1fc3de78fa2673b4
Chenyuxuan <yuntibai02@163.com>
2023-05-27 15:46:01 +0800

    文件质量分析，残余tab和空格格式警告，无视之

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,29 +1,25 @@
 /*任务入口点*/
-#include"Input.cpp"
-#include"SudokuOperation.cpp"
 #include<cstring>
 #include<string>
-using namespace std;
+#include"Input.cpp"
+#include"SudokuOperation.cpp"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
 	Input input(argc, argv);
 	input.input_type_analyse();
 
-	if (input.found_error())
-	{
+	if(input.found_error()){
 	    printf("input error occurs!\n");
 		return 0;
 	}
 
 	char type = input.get_type();
 	int num = input.get_num();
-	int diffculty=input.get_difficulty();
+	int diffculty = input.get_difficulty();
 	char* filename = input.get_filename();
 	
 	SudokuOperation operation;
-	switch(type)
-	{
+	switch(type){
 	case 'c':
 		operation.generate_ending(num, diffculty);
 		break;
